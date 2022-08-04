c808c23f8c517a13952fc7d1e22a69e312462aca
Lucasmrqtt <marquettilu@gmail.com>
2022-08-04 19:20:00 -0300

    Projeto 13

--- sketch.js
+++ sketch.js
@@ -62,15 +62,18 @@ function draw() {
   // var select_balloon = Math.round(random(1,4,2));
   
   if (World.frameCount % 100 == 0) {
-    var sorteio = Math.round(random (1,6))
+    var sorteio = Math.round(random (1,4))
     switch (sorteio) {
-    case 1: pinkBalloon
+    case 1: pinkBalloon();
     break;
-    case 2: blueBalloon
+
+    case 2: blueBalloon();
     break;
-    case 3: redBalloon
+
+    case 3: redBalloon();
     break;
-    case 4: greenBalloon
+
+    case 4: greenBalloon();
     break;
   }
 }
@@ -125,24 +128,24 @@ function draw() {
 
 function blueBalloon() {
   var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  red.addImage(blue_balloonImage);
-  red.velocityX = 3;
-  red.lifetime = 150;
-  red.scale = 0.1;
+  blue.addImage(blue_balloonImage);
+  blue.velocityX = 3;
+  blue.lifetime = 150;
+  blue.scale = 0.1;
 }
 
 function greenBalloon() {
   var green = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  red.addImage(green_balloonImage);
-  red.velocityX = 3;
-  red.lifetime = 150;
-  red.scale = 0.1;
+  green.addImage(green_balloonImage);
+  green.velocityX = 3;
+  green.lifetime = 150;
+  green.scale = 0.1;
 }
 
 function pinkBalloon() {
   var pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
-  red.addImage(pink_balloonImage);
-  red.velocityX = 3;
-  red.lifetime = 150;
-  red.scale = 1;
+  pink.addImage(pink_balloonImage);
+  pink.velocityX = 3;
+  pink.lifetime = 150;
+  pink.scale = 1;
 }
